88cc60f529e5d1946270787599dc114d55b6341f
Md Ariful Shifat <shifat98ariful@gmail.com>
2023-11-22 20:02:14 +0600

    SignIn Page Added

--- DC Office/src/main/SignInPageController.java
+++ DC Office/src/main/SignInPageController.java
@@ -1,30 +1,28 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
 package main;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
 /**
+ * FXML Controller class
  *
  * @author USER
  */
-public class FXMLDocumentController implements Initializable {
-    
+public class SignInPageController implements Initializable {
+
     @FXML
     private Label label;
-    
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO

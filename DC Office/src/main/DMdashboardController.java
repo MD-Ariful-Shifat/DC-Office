ec7e2eced0bf70e6694683e7a75e7ef9190e28f5
tuly-sh <2221132@iub.edu.bd>
2023-11-26 01:51:47 +0600

    updated

--- DC Office/src/main/DMdashboardController.java
+++ DC Office/src/main/DMdashboardController.java
@@ -4,9 +4,18 @@
  */
 package main;
 
+import Model.DM;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,12 +24,41 @@ import javafx.fxml.Initializable;
  */
 public class DMdashboardController implements Initializable {
 
+    private DM user;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+    public DM getUser() {
+        return user;
+    }
+
+    public void setUser(DM user) {
+        this.user = user;
+    }
     
-}
+    @FXML
+    private void signoutOnClick(ActionEvent event) throws IOException {
+        Parent root = null;
+        FXMLLoader someLoader = new FXMLLoader(getClass().getResource("SignIn Page.fxml"));
+        root = (Parent) someLoader.load();
+        Scene someScene = new Scene(root);
+        
+        System.out.println(this.user.toString());
+
+        /*SomeFXMLController p = someLoader.getController();
+        p.setUser(this.user);*/
+
+        Stage someStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        someStage.setScene(someScene);
+        someStage.show();
+
+    }
+
+
+
+    

9516da8066f9af126418de562231e11a2d3c0c28
Sujana169169 <151551712+Sujana169169@users.noreply.github.com>
2023-11-28 14:36:18 +0600

    sujana Updated

--- DC Office/src/main/SignInPageController.java
+++ DC Office/src/main/SignInPageController.java
@@ -164,22 +164,19 @@ public class SignInPageController implements Initializable {
         DC dc2 = new DC(101, "password", "Shifat", "Bashundhara", "0170101010", date1, "Chittagong");
         DC dc3 = new DC(102, "password", "Shifat", "Bashundhara", "0170101010", date1, "Dhaka");
         SAC sac1 = new SAC(200, "word", "Shifat", "Bashundhara", "0170101010", date1, "Dhaka");
+        
         SAC sac2 = new SAC(201, "password", "Shifat", "Bashundhara", "0170101010", date1, "Dhaka");
         SAC sac3 = new SAC(202, "password", "Shifat", "Bashundhara", "0170101010", date1, "Dhaka");
-<<<<<<< HEAD
-        AA aa1 = new AA(300, "password", "Shujana", "Bashundhara", "0170101010", date1, "Dhaka");
-        AA aa2 = new AA(301, "password", "Shujana", "Bashundhara", "0170101010", date1, "Dhaka");
-        AA aa3 = new AA(302, "password", "Shujana", "Bashundhara", "0170101010", date1, "Dhaka");
-=======
-        AA aa1 = new AA(301, "password", "Suzana", "Bashundhara", "0170101010", date1, "Dhaka");
-        EI ei1 = new EI(302, "password", "Suzana", "Bashundhara", "0170101010", date1, "Dhaka");
+
+        AA aa1 = new AA(301, "password", "Sujana", "Bashundhara", "0170101010", date1, "Dhaka");
+        EI ei1 = new EI(302, "password", "Sujana", "Bashundhara", "0170101010", date1, "Dhaka");
+        
         DM dm1 = new DM(401, "password", "Sadia", "Bashundhara", "0170101010", date1, "Dhaka");
         SACEB saceb1 = new SACEB(402, "password", "Sadia", "Bashundhara", "0170101010", date1, "Dhaka");
 
 
 
->>>>>>> c9ca8deb3e1025ac1f8944cac8cc30ac16ac8fe2
-        
+
         
         File f = null;
         FileOutputStream fos = null;      
@@ -201,14 +198,11 @@ public class SignInPageController implements Initializable {
             oos.writeObject(sac2);
             oos.writeObject(sac3);
             oos.writeObject(aa1);
-<<<<<<< HEAD
-            oos.writeObject(aa2);
-            oos.writeObject(aa3);
-=======
+
             oos.writeObject(ei1);
             oos.writeObject(dm1);
             oos.writeObject(saceb1);
->>>>>>> c9ca8deb3e1025ac1f8944cac8cc30ac16ac8fe2
+
             oos.close();
 
         } catch (IOException ex) {} 

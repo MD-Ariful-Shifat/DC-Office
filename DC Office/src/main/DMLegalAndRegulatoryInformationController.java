fbd4bf661c29c23adf463a344614ec9a274de860
tuly-sh <2221132@iub.edu.bd>
2023-12-01 01:46:32 +0600

    update

--- DC Office/src/main/DMLegalAndRegulatoryInformationController.java
+++ DC Office/src/main/DMLegalAndRegulatoryInformationController.java
@@ -4,9 +4,17 @@
  */
 package main;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -22,5 +30,15 @@ public class DMLegalAndRegulatoryInformationController implements Initializable 
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void backOnClick(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("DMdashboard.fxml"));
+                Scene scene = new Scene(root);
+                Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                window.setTitle("DMdashboard");
+                window.setScene(scene);
+                window.show();
+    }
     
 }

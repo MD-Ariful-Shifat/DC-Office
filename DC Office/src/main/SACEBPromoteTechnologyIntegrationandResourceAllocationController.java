fd7bbfccf4f4fba9b3e9ad0950ce288cdde2ce8f
tuly-sh <2221132@iub.edu.bd>
2023-12-01 01:03:50 +0600

    Update SACEBPromoteTechnologyIntegrationandResourceAllocationController.java

--- DC Office/src/main/SACEBPromoteTechnologyIntegrationandResourceAllocationController.java
+++ DC Office/src/main/SACEBPromoteTechnologyIntegrationandResourceAllocationController.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
@@ -32,20 +33,14 @@ public class SACEBPromoteTechnologyIntegrationandResourceAllocationController im
         // TODO
     }    
 
+    @FXML
         private void backOnClick(ActionEvent event) throws IOException {
-        Parent root = null;
-        FXMLLoader someLoader = new FXMLLoader(getClass().getResource("SACEBdashboard.fxml"));
-        root = (Parent) someLoader.load();
-        Scene someScene = new Scene(root);
-        
-        System.out.println(this.user.toString());
-
-        /*SomeFXMLController p = someLoader.getController();
-        p.setUser(this.user);*/
-
-        Stage someStage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        someStage.setScene(someScene);
-        someStage.show();
+            Parent root = FXMLLoader.load(getClass().getResource("SACEBdashboard.fxml"));
+                Scene scene = new Scene(root);
+                Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                window.setTitle("SACEBdashboard");
+                window.setScene(scene);
+                window.show();
     }
     
 }

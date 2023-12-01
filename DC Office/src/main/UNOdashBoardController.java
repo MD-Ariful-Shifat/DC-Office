b1dbb29ac7b5606aa50ff647c00f1a7f63e3a3a4
Israt-Jahan-Sujana <isratsujana25@gmail.com>
2023-12-02 01:17:47 +0600

    UNOdashboardUpdate

---
+++ DC Office/src/main/UNOdashBoardController.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package main;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author ASUS
+ */
+public class UNOdashBoardController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void BackBtnonClick(MouseEvent event) throws IOException {
+        
+        Parent parent = FXMLLoader.load(getClass().getResource("SignIn Page.fxml"));
+         Scene scene = new Scene(parent);
+         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+         window.setScene(scene);
+         window.show();
+        
+    }
+    
+}

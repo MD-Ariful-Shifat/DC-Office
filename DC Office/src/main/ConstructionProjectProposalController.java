3c4902e1f4d80da0143cfc20b4f9fc3a8dc0ecb9
Israt-Jahan-Sujana <isratsujana25@gmail.com>
2023-12-02 13:14:03 +0600

    updated

--- DC Office/src/main/ConstructionProjectProposalController.java
+++ DC Office/src/main/ConstructionProjectProposalController.java
@@ -4,6 +4,8 @@
  */
 package main;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,6 +15,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -21,7 +24,10 @@ import javafx.stage.Stage;
  *
  * @author ASUS
  */
-public class DrroDashboardController implements Initializable {
+public class ConstructionProjectProposalController implements Initializable {
+
+    @FXML
+    private TextField ProposalTextField;
 
     /**
      * Initializes the controller class.
@@ -32,34 +38,29 @@ public class DrroDashboardController implements Initializable {
     }    
 
     @FXML
-    private void AllocateFoodandCashBtn(MouseEvent event) throws IOException {
+    private void SaveBtnOnClick(MouseEvent event) {
         
-        Parent parent = FXMLLoader.load(getClass().getResource("AllocateFoodCash.fxml"));
-         Scene scene = new Scene(parent);
-         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-         window.setScene(scene);
-         window.show();
-        
+        String content = ProposalTextField.getText() ;
 
+    // The file to write to
+    File outputFile = new File("Proposal.txt");
+
+    try {
+        FileWriter writer = new FileWriter(outputFile);
+        writer.write(content);
+        writer.close();
+        System.out.println("Proposal written");
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+        // TODO
         
     }
 
     @FXML
     private void BackBtnOnClick(MouseEvent event) throws IOException {
         
-        Parent parent = FXMLLoader.load(getClass().getResource("SignIn Page.fxml"));
-         Scene scene = new Scene(parent);
-         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-         window.setScene(scene);
-         window.show();
-        
-
-        
-    }
-
-    @FXML
-    private void BridgeProjectonClick(MouseEvent event) throws IOException {
-        Parent parent = FXMLLoader.load(getClass().getResource("ConstructionProjectProposal.fxml"));
+        Parent parent = FXMLLoader.load(getClass().getResource("DrroDashboard.fxml"));
          Scene scene = new Scene(parent);
          Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
          window.setScene(scene);
